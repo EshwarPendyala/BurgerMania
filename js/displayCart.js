c87d09521bdf8394b67bad9f63e932e173105ee7
Eshwar Pendyala <eshwar.pendyala16@vit.edu>
2020-04-27 19:13:13 +0530

    Update displayCart.js

--- js/displayCart.js
+++ js/displayCart.js
@@ -5,6 +5,7 @@ function $(id){
 
 burgerList = JSON.parse(burgerArray);
 var cartTable = $("cartTable");
+var td6;
 for (var i = 0; i < burgerList.length; i++) {
     // var tr = "<tr>";
     var tr = cartTable.insertRow(i);
@@ -18,8 +19,25 @@ for (var i = 0; i < burgerList.length; i++) {
     td4.innerHTML = burgerList[i].burgerPrice;
     var td5 = tr.insertCell(4);
     td5.innerHTML = burgerList[i].burgerPrice * burgerList[i].burgerQuantity;
-    
+    var td6 = tr.insertCell(5);
+    td6.innerHTML = "X";
 }
+
+var cells = document.getElementsByTagName('td');
+for(var i=0; i<cells.length;i++)
+{
+    var cell = cells[i];
+    if (cell.cellIndex === 5) {
+    cell.onclick = function(){
+        
+            var delIndex = this.parentNode.rowIndex;
+            burgerList.slice(delIndex);
+            alert(burgerList);
+            $('cartTable').deleteRow(this.parentNode.rowIndex);
+        }
+    }
+}
+
 var quantity = 0;
 var price = 0;
 
